5f0d499759b6f9a4438ecdc32f5c83911cf3a957
heyu <576513298@qq.com>
2019-09-15 10:12:06 +0800

    third commit

--- DemoProject/src/main/java/com/hy/util/Constant.java
+++ DemoProject/src/main/java/com/hy/util/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 
  public static final String PURGE = "top of it xxx" ;
 
- public static final String XINWUKA = "top of it xxx" ;
+ public  final String XINWUKA = "top of it xxx" ;
 
 //11111
 }
